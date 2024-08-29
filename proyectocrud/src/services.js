cae789f9b13adc107556166996775018fad4a1c9
magalylazarte <magvita8@gmail.com>
2024-08-29 13:49:24 +0200

    practicacrud

--- proyectocrud/src/services.js
+++ proyectocrud/src/services.js
@@ -1,56 +1,85 @@
-const URL_API = "http://localhost:3000/zoo"
+const URL_API = "http://localhost:3000/character"
+console.log("hola")
 // son peticiones al servidor 
 //read method : (get)
-async function getAllZoo() {
- const response = await fetch("URL_API",{
+async function getAllCharacters() {
+ const response = await fetch(URL_API,{
     method: "GET",
     headers: {
         "Content-Type": "application/json",
     },
 })
-const data = await response.json() 
+const data = await response.json()  //pasar datos al formato
 return data
 
 }
-const listTag = document.getElementById("characterlist"),
-async function printZoo () {
-    const zoo = await getAllZoo()
-    zoo.map ((zoo)) => {
-        listTag.innerHTML += `<li>
-        <p>${zoo.nomnbre}</p>
-        <p>${zoo.especie}</p>
-        <p>${zoo.años}</p>
-        <button onclick= "deletecharacter(${chatacter.id})">delete</button>
+
+const listTag = document.getElementById("charactersList")  
+
+async function printCharacter () {
+    
+    const zoo = await getAllCharacters()
+   
+    //variable.innerHTML= "" 
+    
+    zoo.map ((animal) => {
+        listTag.innerHTML +=
+        `<li>
+        <p>${animal.nombre}</p>
+        <p>${animal.especie}</p>
+        <p>${animal.años}</p>
+    <button onclick= "deleteCharacter(${animal.id})">delete</button>
         </li>`
 
-    }
-    
+    })
+
 }
+printCharacter ()
 
 //delete method : (delete)
-async function deleteZoo() {
-    const response =await fetch(URL_API + "/${id}", {
-        meyhod : "DELETE",
-        headers {
-            "content-Type": "apllication/json",
-
+async function deleteCharacter(id) {
+    const response = await fetch(URL_API + `/${id}` , { 
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json",
         },
-
-    })  
-    const deletedZoo = await response.json()
-    console.log(deleteZoo)
-    return deleteZoo 
-
+    })
+    const deletedCharacter = await response.json()
+    return deletedCharacter
 }
 //create method : (post)
 
-async function createZoo() {
+async function updateCharacter(id) {
+    const nombre = document.getElementById("nombre").value 
+    const especie = document.getElementById ("especie").value 
+    const años = document.getElementById("años").value 
 
+    const updatedAnimal = {
+        nombre: nombre,
+        especie: especie,
+        años: años
+    }
+
+    const response = await fetch(URL_API + `/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedAnimal),
+    })
+
+    if (response.ok) {
+        const data = await response.json();
+        // Actualizar la interfaz de usuario según los cambios
+        printCharacter();  // Volver a cargar la lista de personajes
+    }
 }
 //update method : (put)
-async function updateZoo() {
+async function updateCharacter() {
+
 
 }
+const addAnimalForm= document.getElementById('createCharacterForm')
 
   
 
