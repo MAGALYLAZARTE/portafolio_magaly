4d23878c6625c4141118c834dc28148dd7a145a9
magalylazarte <magvita8@gmail.com>
2024-08-29 14:45:12 +0200

    repo3

--- proyectocrud/src/services.js
+++ proyectocrud/src/services.js
@@ -49,35 +49,50 @@ async function deleteCharacter(id) {
 }
 //create method : (post)
 
-async function updateCharacter(id) {
-    const nombre = document.getElementById("nombre").value 
-    const especie = document.getElementById ("especie").value 
-    const años = document.getElementById("años").value 
+async function createCharacter() {
+    const nombre = document.getElementById("nombre").value;
+    const especie = document.getElementById("especie").value;
+    const años = document.getElementById("años").value;
 
-    const updatedAnimal = {
+    const newAnimal = {
         nombre: nombre,
         especie: especie,
         años: años
-    }
+    };
 
-    const response = await fetch(URL_API + `/${id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(updatedAnimal),
-    })
+    try {
+        const response = await fetch(URL_API, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newAnimal)
+        });
 
-    if (response.ok) {
-        const data = await response.json();
-        // Actualizar la interfaz de usuario según los cambios
-        printCharacter();  // Volver a cargar la lista de personajes
+        if (response.ok) {
+            const data = await response.json(); 
+            addAnimal(data);
+        } else {
+            console.error('Error en la respuesta:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error en la solicitud:', error);
     }
 }
+
+async function addAnimal(animal) {
+    const characterAnimal = document.getElementById('charactersList');
+    const listAnimal = document.createElement('li');
+    listAnimal.textContent = `${animal.nombre}, ${animal.especie}, ${animal.años}`;
+
+    characterAnimal.appendChild(listAnimal);
+}
+
+
 //update method : (put)
 async function updateCharacter() {
 
-
+    
 }
 const addAnimalForm= document.getElementById('createCharacterForm')
 
